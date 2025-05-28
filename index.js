4da57fc1d463743e35026125f96f144940ff257e
Manu2021-U <manu.mudavath03@gmail.com>
2025-05-29 01:07:29 +1200

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ require('dotenv').config()
 app.use(express.json());
 app.use(cors({
 
-    origin: ['http://localhost:5173','https://frontend-two-ecru-14.vercel.app'],
+    origin: ['http://localhost:5173','https://frontend-f4bc.vercel.app'],
     credentials: true
 }))
 
